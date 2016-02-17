eb20ed1e77c8e055acff0ff73d1e1cacbea29cbe
Sonia Putzel <sonia.putzel@ogilvy.com>
2016-02-17 18:07:23 -0500

    added new files, still working on setup

---
+++ src/js/es6/main.js
@@ -0,0 +1,6 @@
+// Main application entry point
+import 'babelify/polyfill';	
+import LeapKeyboard from './components/LeapKeyboard';
+
+// Instantiate
+window.LeapKeyboard = window.LeapKeyboard || LeapKeyboard;
