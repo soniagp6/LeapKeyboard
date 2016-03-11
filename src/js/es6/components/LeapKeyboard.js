f9f08f360c4fad01f44b7735840819129580559f
Sonia Putzel <sonia.putzel@ogilvy.com>
2016-03-11 17:56:09 -0500

    trying to make javascript run

--- src/js/es6/components/LeapKeyboard.js
+++ src/js/es6/components/LeapKeyboard.js
@@ -1,7 +1,5 @@
-import Leap from 'http://js.leapmotion.com/leap-0.6.3.min.js'
-
 export default class LeapKeyboard {
 	constructor() {
-
+		console.log('test');
 	}
 }
--- src/js/es6/main.js
+++ src/js/es6/main.js
@@ -1,5 +1,2 @@
 // Main application entry point
 import LeapKeyboard from './components/LeapKeyboard';
-
-// Instantiate
-window.LeapKeyboard = window.LeapKeyboard || LeapKeyboard;
